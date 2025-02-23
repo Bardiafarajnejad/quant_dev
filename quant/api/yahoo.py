af30b7597af5f1c131a75b0ab96eb33fb89a41ef
Bardia Farajnejad <136946919+bobby2324@users.noreply.github.com>
2025-02-22 19:14:41 -0600

    black file

--- quant/api/yahoo.py
+++ quant/api/yahoo.py
@@ -1,11 +1,36 @@
 import yfinance as yf
 import pandas as pd
 from typing import Literal, Union
-def ydh(tickers: Union[str, list[str]],
-        start_date: pd.Timestamp = pd.Timestamp(1980,1,1).floor("D"),
-        end_date: pd.Timestamp = pd.Timestamp.now(),
-        frequency: Literal["1m","2m","5m","15m","30m","60m","90m","1h","1d","5d","1wk","1mo","3mo"] = '1d',
-        threads: bool = False) -> pd.DataFrame:
-    
-    df = yf.download(tickers=tickers, start=start_date, end=end_date, interval=frequency, threads=threads, group_by='tickers')
+
+
+def ydh(
+    tickers: Union[str, list[str]],
+    start_date: pd.Timestamp = pd.Timestamp(1980, 1, 1).floor("D"),
+    end_date: pd.Timestamp = pd.Timestamp.now(),
+    frequency: Literal[
+        "1m",
+        "2m",
+        "5m",
+        "15m",
+        "30m",
+        "60m",
+        "90m",
+        "1h",
+        "1d",
+        "5d",
+        "1wk",
+        "1mo",
+        "3mo",
+    ] = "1d",
+    threads: bool = False,
+) -> pd.DataFrame:
+
+    df = yf.download(
+        tickers=tickers,
+        start=start_date,
+        end=end_date,
+        interval=frequency,
+        threads=threads,
+        group_by="tickers",
+    )
     return df
