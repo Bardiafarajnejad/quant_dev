dbc6ebb3684d3749071de0b4620f3f608e9f9772
Bardiafarajnejad <116606534+Bardiafarajnejad@users.noreply.github.com>
2025-02-23 21:16:50 -0600

    Add fields to ydh (#8)
    * Update yahoo.py
    
    * Update yahoo.py
    
    * Update yahoo.py
    
    * Update yahoo.py
    
    * Update yahoo.py
    
    * Update yahoo.py
    
    * Update yahoo.py
    
    * black file

--- quant/api/yahoo.py
+++ quant/api/yahoo.py
@@ -1,27 +1,32 @@
 import yfinance as yf
 import pandas as pd
-from typing import Literal, Union
+from typing import Literal, Union, Optional
+
+YAHOO_FREQUENCIES = Literal[
+    "1m",
+    "2m",
+    "5m",
+    "15m",
+    "30m",
+    "60m",
+    "90m",
+    "1h",
+    "1d",
+    "5d",
+    "1wk",
+    "1mo",
+    "3mo",
+]
+
+YAHOO_FIELDS = Literal["Open", "High", "Low", "Close", "Adj Close", "Volume"]
 
 
 def ydh(
     tickers: Union[str, list[str]],
+    fields: Optional[Union[YAHOO_FIELDS, list[YAHOO_FIELDS]]] = None,
     start_date: pd.Timestamp = pd.Timestamp(1980, 1, 1).floor("D"),
     end_date: pd.Timestamp = pd.Timestamp.now(),
-    frequency: Literal[
-        "1m",
-        "2m",
-        "5m",
-        "15m",
-        "30m",
-        "60m",
-        "90m",
-        "1h",
-        "1d",
-        "5d",
-        "1wk",
-        "1mo",
-        "3mo",
-    ] = "1d",
+    frequency: YAHOO_FREQUENCIES = "1d",
     threads: bool = False,
 ) -> pd.DataFrame:
 
@@ -34,4 +39,14 @@ def ydh(
         group_by="tickers",
         auto_adjust=False,
     )
+    if isinstance(fields, str):
+        column_mask = df.columns.get_level_values(1).isin([fields])
+        df = df.iloc[:, column_mask].T.reset_index(level=1, drop=True).T
+    elif isinstance(fields, list):
+        column_mask = df.columns.get_level_values(1).isin(fields)
+        df = df.iloc[:, column_mask]
+    elif fields is None:
+        pass
+    else:
+        raise TypeError(f"Unexpected fields type: {fields}")
     return df
